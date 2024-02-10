9d7e5dd0801161cea5bfc90d94603f634710758a
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-02-11 01:29:43 +0530

    Plans Section created

--- User/src/Components/AboutAlamVillas/Aoutalamvillas.jsx
+++ User/src/Components/AboutAlamVillas/Aoutalamvillas.jsx
@@ -1,21 +1,113 @@
-import React from "react";
-import { Container } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import "../AboutAlamVillas/Aboutalavillas.css";
-import bgcardlogo from "../../assets/img/bg-card-logo.png"
+import bgcardlogo from "../../assets/img/bg-card-logo.png";
+import About from "../AboutUs/About";
+import Project1 from "../../assets/img/Amenities/New-Project-1.jpg";
+import Project2 from "../../assets/img/Amenities/New-Project-2.jpg";
+import Project3 from "../../assets/img/Amenities/New-Project-3.jpg";
+import Project4 from "../../assets/img/Amenities/New-Project-4.jpg";
+import Project5 from "../../assets/img/Amenities/New-Project-5.jpg";
+import ProjectPlans from "../ProjectsPlan/ProjectsPlan";
+
+const cardsData = [
+  {
+    id: 1,
+    name: "Club House",
+    img: Project1,
+  },
+  {
+    id: 2,
+    name: "Swimming Pool",
+    img: Project2,
+  },
+  {
+    id: 3,
+    name: "Play Ground",
+    img: Project3,
+  },
+  {
+    id: 4,
+    name: "Designer Landscape",
+    img: Project4,
+  },
+  {
+    id: 5,
+    name: "Play Area",
+    img: Project5,
+  },
+];
 
 const Aoutalamvillas = () => {
   return (
-    <div className="about-ala-logo">
-      {/* <div className="img-contain">
-        <img src={bgcardlogo} alt="" />
-      </div> */}
-      <div className="text-area">
-        <h5>Magnificent living lies ahead. </h5>
-        <h2>
-          Step into the new <br />
-          Heights of grandeur
-        </h2>
+    <div className="">
+      <div className="img-contain">
+        <div className="text-area">
+          <h5>Magnificent living lies ahead. </h5>
+          <h2>
+            Step into the new <br />
+            Heights of grandeur
+          </h2>
+        </div>
       </div>
+      <Container className="aboutContainer">
+        <About />
+      </Container>
+      <Container className="CardsContainer">
+        <Row>
+          <h1>AMENITIES</h1>
+        </Row>
+        <Row>
+          <p>A WORLD OF WONDERS</p>
+        </Row>
+        <Row className="pb-4 CardsContainer">
+          {cardsData.slice(0, 3).map((items, index) => (
+            <Col className={index} xs={"11"} sm={"6"} md={"4"} key={items.id}>
+              <div
+                className="card-2"
+                style={{ backgroundImage: `url(${items.img})` }}
+              >
+                {/* <img src={items.img} alt="" /> */}
+                <div className="heading-holder">
+                  <h1>{items.name}</h1>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+        <Row className="pb-4 CardsContainer">
+          {cardsData.slice(3, 5).map((items, index) => (
+            <Col xs={"11"} sm={"6"} lg={"6"} md={"6"}>
+              <div
+                className="card-2"
+                style={{ backgroundImage: `url(${items.img})` }}
+              >
+                <div className="heading-holder">
+                  <h1>{items.name}</h1>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <h1>PLANS</h1>
+        </Row>
+        <Row>
+          <p>PROJECT PLANS</p>
+        </Row>
+        <Row className="CardsContainer">
+          <ProjectPlans />
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ User/src/Components/ProjectsPlan/ProjectsPlan.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+
+import New1 from "../../assets/img/Plans/New-1.png";
+import New2 from "../../assets/img/Plans/New-2.png";
+import New3 from "../../assets/img/Plans/New-3.jpg";
+
+const slides = [
+  {
+    url: New1,
+    title: "Slide 1"
+  },   
+  {
+    url: New2,
+    title: "Slide 2"
+  },
+  {
+    url: New3,
+    title: "Slide 3"
+  },
+];
+
+const slideStyles = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "10px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+const rightArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  right: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const leftArrowStyles = {
+  position: "absolute",
+  top: "50%",
+  transform: "translate(0, -50%)",
+  left: "32px",
+  fontSize: "45px",
+  color: "#fff",
+  zIndex: 1,
+  cursor: "pointer",
+};
+
+const sliderStyles = {
+  position: "relative",
+  height: "100%",
+};
+
+const dotsContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+};
+
+const dotStyle = {
+  margin: "0 3px",
+  cursor: "pointer",
+  fontSize: "20px",
+};
+
+const ProjectsPlan = ({ slides }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const goToPrevious = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+    };
+    const goToNext = () => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    };
+    const goToSlide = (slideIndex) => {
+      setCurrentIndex(slideIndex);
+    };
+    const slideStylesWidthBackground = {
+      ...slideStyles,
+      backgroundImage: `url(${slides[currentIndex].url})`,
+    };
+  
+    return (
+      <div style={sliderStyles}>
+        <div>
+          <div onClick={goToPrevious} style={leftArrowStyles}>
+            ❰
+          </div>
+          <div onClick={goToNext} style={rightArrowStyles}>
+            ❱
+          </div>
+        </div>
+        <div style={slideStylesWidthBackground}></div>
+        <div style={dotsContainerStyles}>
+          {slides.map((slide, slideIndex) => (
+            <div
+              style={dotStyle}
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+            >
+              ●
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default ProjectsPlan;
